99db202506e1d298eeeecbf5f8fc33b3710b63d4
Stefan Angelov <stefanangelov@deltasource.eu>
2016-03-28 11:08:44 +0300

    Sum Lines

--- StreamsAndFiles/src/com/cefothe/softuni/SumLines.java
+++ StreamsAndFiles/src/com/cefothe/softuni/SumLines.java
@@ -1,9 +1,25 @@
 package com.cefothe.softuni;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class SumLines {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+
+		try (BufferedReader reader = new BufferedReader(new FileReader("resources/line.txt"))) {
+			String line = reader.readLine();
+			while (line != null) {
+				int symbolTotalCount = 0;
+				for (int i = 0; i < line.length(); i++) {
+					symbolTotalCount += line.charAt(i);
+				}
+				System.out.println(symbolTotalCount);
+				line = reader.readLine();
+			}
+		}
 
 	}
 
