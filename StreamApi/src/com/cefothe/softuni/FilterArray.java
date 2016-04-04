1ea54515710868bb724fb7e0017a3c32acee57b8
Stefan Angelov <stefanangelov@deltasource.eu>
2016-04-04 22:46:57 +0300

    save

---
+++ StreamApi/src/com/cefothe/softuni/FilterArray.java
@@ -0,0 +1,13 @@
+package com.cefothe.softuni;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FilterArray {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String[] words = scanner.nextLine().split("\\s+");
+		 Arrays.stream(words)args;
+	}
+}
