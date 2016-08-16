cea25366466c52c206932bf1ad72bb7c68e21aff
Stefan Angelov <stefanangelov@deltasource.eu>
2016-08-16 11:45:39 +0300

    OCA

--- Test/src/B.java
+++ Test/src/B.java
@@ -1,19 +1,14 @@
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 /**
  * Created by stefanangelov on 8/12/16.
  */
-class A{
-    static {
-        System.out.println("A");
-    }
-    A() {}
-}
-class B extends A {
-    static  int i=1_______3;
-    static {
-        System.out.println(i);
-    }
+
+class B {
     public static void main(String[] args) {
+        char c ='\u0000';
+
+        System.out.print(c==0);
     }
 }
