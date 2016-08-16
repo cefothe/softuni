cc8958cfb0930b087cdebd9133ffcddcf0d3d388
Stefan Angelov <stefanangelov@deltasource.eu>
2016-08-16 12:53:23 +0300

    Implement method, that return html

--- JavaWebDevelopment/ServletsAndPages/src/com/cefothe/servlets/DynamicContent.java
+++ JavaWebDevelopment/ServletsAndPages/src/com/cefothe/servlets/DynamicContent.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 /**
  * Created by stefanangelov on 8/15/16.
@@ -30,9 +31,21 @@ public class DynamicContent extends HttpServlet {
         }
 
         // Return counter to response
-        resp.getWriter().append(count.toString());
+        resp.getWriter().append(formatDynamicContent(count));
 
         //set attribute "COUNTER" to req
         req.getSession().setAttribute("COUNTER",++count);
+    }
+
+    private String formatDynamicContent(int count){
+        StringBuilder builder = new StringBuilder();
+        builder.append("<html>");
+        builder.append("<head><title>").append("Dynamic Content").append("</title></head>");
+        builder.append("<body>");
+        builder.append("<h2>Timestamp:").append(LocalDateTime.now()).append("</h2><br>");
+        builder.append("<h3>Times:").append(count).append("</h3>");
+        builder.append("</body>");
+        builder.append("</html>");
+        return  builder.toString();
     }
 }
