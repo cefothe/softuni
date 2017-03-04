63b784078999e1b171f057300d4a0cf20a532fda
Stefan Angelov <cefothe@gmail.com>
2017-03-04 02:19:01 +0200

    Servlet

---
+++ JavaWebDevelopmentBasic/servlet-lecture/src/main/java/servlet/PrintBeerServlet.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by cefothe on 04.03.17.
+ */
+@WebServlet("/beer")
+public class PrintBeerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter printWriter = resp.getWriter();
+        HttpSession session = req.getSession();
+        printWriter.println(session.getAttribute("username"));
+    }
+}
